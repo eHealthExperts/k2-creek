90ebce33f52f533ccd63d10fb1497ea30b70e8f6
Benjamin Wenzel <b.wenzel@mail.de>
2017-12-03 00:39:15 +0100

    Error handling and refactoring of config loading

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -8,16 +8,21 @@ const DEFAULT_PORT : &'static str = "5000";
 const DEFAULT_PATH : &'static str = "/k2/public/api/1/carddata";
 
 pub fn generate_url_from_config() -> String {
-    let conf = Ini::load_from_file("config.ini").unwrap();
-    let section = conf.section(Some("settings".to_owned())).unwrap();
-    let def_scheme = DEFAULT_SCHEME.to_owned();
-    let scheme = section.get("scheme").unwrap_or(&def_scheme);
-    let def_host = DEFAULT_HOST.to_owned();
-    let host = section.get("host").unwrap_or(&def_host);
-    let def_port = DEFAULT_PORT.to_owned();
-    let port = section.get("port").unwrap_or(&def_port);
-    let def_path = DEFAULT_PATH.to_owned();
-    let path = section.get("path").unwrap_or(&def_path);
+    let cfg;
+    let mut scheme = DEFAULT_SCHEME;
+    let mut host = DEFAULT_HOST;
+    let mut port = DEFAULT_PORT;
+    let mut path = DEFAULT_PATH;
+    if let Ok(conf) = Ini::load_from_file("config.ini") {
+        cfg = conf;
+        if let Some(section) = cfg.section(Some("settings".to_owned())) {
+            if let Some(config_val) = section.get("scheme") { scheme = config_val }
+            if let Some(config_val) = section.get("host") { host = config_val }
+            if let Some(config_val) = section.get("port") { port = config_val }
+            if let Some(config_val) = section.get("path") { path = config_val }
+        }
+    }
+
     let url = format!("{}://{}:{}{}", scheme, host, port, path);
     url.to_owned()
 }
