e7245bade6b2040d933a88aa71322a3d86aa375a
Christian Fochler <mail@christianfochler.de>
2018-01-19 16:25:21 +0100

    Rewrite config mod

---
+++ src/config.rs
@@ -0,0 +1,47 @@
+macro_rules! replace_by_ini {
+    ($ini:ident, $target:expr,$config_key:expr) => (
+        if let Some(val) = $ini.get_from(Some("settings"), $config_key) {
+            $target  = val.to_owned();
+        }
+    )
+}
+
+pub struct Config {
+    scheme: String,
+    host: String,
+    port: String,
+    path: String,
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            scheme: String::from("http"),
+            host: String::from("localhost"),
+            port: String::from("5000"),
+            path: String::from("/k2/public/api/1/carddata"),
+        }
+    }
+}
+
+impl Config {
+    pub fn new() -> Config {
+        let mut config: Config = Default::default();
+
+        if let Ok(ini) = ::ini::Ini::load_from_file("config.ini") {
+            replace_by_ini!(ini, config.scheme, "scheme");
+            replace_by_ini!(ini, config.host, "host");
+            replace_by_ini!(ini, config.port, "port");
+            replace_by_ini!(ini, config.path, "path");
+        }
+
+        config
+    }
+
+    pub fn to_url(&self) -> String {
+        format!(
+            "{}://{}:{}{}",
+            &self.scheme, &self.host, &self.port, &self.path
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-#[macro_use] extern crate serde_derive;
+extern crate ini;
+#[macro_use]
+extern crate serde_derive;
 
 mod config;
 mod file_writer;
 mod request;
 
-use config::generate_url_from_config;
 use file_writer::dump_egk_data_to_files;
 use request::request_egk_data;
 
 fn main() {
-    let url = generate_url_from_config();
+    let config = config::Config::new();
+    let url = config.to_url();
     println!("Retrieving data from {}", &url);
 
     let resp = request_egk_data(&url).unwrap();
