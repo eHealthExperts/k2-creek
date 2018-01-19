0cc0407c2db7d93090702f0606379fbddff2ae24
Christian Fochler <mail@christianfochler.de>
2018-01-19 16:31:08 +0100

    adjust request mod

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate ini;
+extern crate reqwest;
 #[macro_use]
 extern crate serde_derive;
 
@@ -7,14 +8,14 @@ mod file_writer;
 mod request;
 
 use file_writer::dump_egk_data_to_files;
-use request::request_egk_data;
 
 fn main() {
     let config = config::Config::new();
     let url = config.to_url();
     println!("Retrieving data from {}", &url);
 
-    let resp = request_egk_data(&url).unwrap();
+    let res = request::request_egk_data(&url);
+    println!("Processing response...");
 
-    dump_egk_data_to_files(&resp);
+    dump_egk_data_to_files(&res);
 }
